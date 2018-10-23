76b2254daa208bf921d802e180e0b49f3074f884
orangutan <anonymousid@veryprivate.com>
2018-10-23 10:57:14 +0530

    initital settings
    Change-Id: I6d3428f5f0dddfd13dad915ef1e157918885f7bf

---
+++ Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := privacybrowser
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := privacybrowser
+
+pb_root  := $(LOCAL_PATH)
+pb_dir   := app
+pb_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
+pb_build := $(pb_root)/$(pb_dir)/build
+pb_apk   := build/outputs/apk/standard/release/app-standard-release-unsigned.apk
+
+$(pb_root)/$(pb_dir)/$(pb_apk):
+	rm -Rf $(pb_build)
+	mkdir -p $(pb_out)
+	ln -sf $(pb_out) $(pb_build)
+	cd $(pb_root)/$(pb_dir) && ../gradlew assembleRelease
+
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := $(pb_dir)/$(pb_apk)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+include $(BUILD_PREBUILT)
